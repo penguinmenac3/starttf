ddbb7663cb7b95c673bf06e305fd5789da5be671
Michael Fuerst <mail@michaelfuerst.de>
2018-01-11 05:20:27 +0100

    Created a sample how lfw could be implemented if a model would exist.

---
+++ examples/lfw_example.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+from datasets.classification.named_folders import named_folders
+from models.model import Model
+
+
+def main():
+    hyper_params_filepath = "examples/lfw.json"
+
+    print("Load Dataset")
+    roi = (200, 200)
+    data = named_folders("data/lfw-deepfunneled", phase=None, crop_roi=roi, file_extension=".jpg")
+
+    print("Create model")
+    # TODO use a real model
+    model = Model(hyper_params_filepath)
+
+    # Define how much gpu memory to allow for training.
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.75
+    with tf.Session(config=config) as sess:
+        print("Setup model")
+        model.setup(sess)
+
+        print("Fit model")
+        model.fit(training_data=data, iters=50000)
+
+        print("Export model")
+        model.export()
+
+
+if __name__ == "__main__":
+    main()
