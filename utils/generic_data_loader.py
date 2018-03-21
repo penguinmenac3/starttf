2455612ea09ce7368debc32677c29707a6987356
Michael Fuerst <fuerst@fzi.de>
2018-03-21 15:59:24 +0100

    Removed validation dataset size but introduced separate batch size for validation. Since for large data you cannot use all in a single batch size.

--- utils/generic_data_loader.py
+++ utils/generic_data_loader.py
@@ -2,7 +2,7 @@ import os
 from datasets.tfrecords import write_tf_records, read_tf_records, PHASE_TRAIN, PHASE_VALIDATION
 
 
-def load_data(hyper_params, generate_data_fn, data_tmp_folder, force_generate_data=False, validation_examples_number=-1):
+def load_data(hyper_params, generate_data_fn, data_tmp_folder, force_generate_data=False):
     if force_generate_data or not os.path.exists(data_tmp_folder):
         if not os.path.exists(data_tmp_folder):
             os.makedirs(data_tmp_folder)
@@ -13,10 +13,7 @@ def load_data(hyper_params, generate_data_fn, data_tmp_folder, force_generate_da
         write_tf_records(data_tmp_folder, 4, 2, train_data, validation_data)
 
     # Load data with tf records.
-    if validation_examples_number < 0:
-        validation_examples_number = hyper_params.train.batch_size
     train_features, train_labels = read_tf_records(data_tmp_folder, PHASE_TRAIN, hyper_params.train.batch_size)
-    validation_features, validation_labels = read_tf_records(data_tmp_folder, PHASE_VALIDATION,
-                                                             validation_examples_number)
+    validation_features, validation_labels = read_tf_records(data_tmp_folder, PHASE_VALIDATION, hyper_params.train.validation_batch_size)
 
     return train_features, train_labels, validation_features, validation_labels
