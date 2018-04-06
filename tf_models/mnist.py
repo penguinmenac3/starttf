b1e8445d27b5d474ffaf65f64cd8dd76c416f2f1
Michael Fuerst <mail@michaelfuerst.de>
2018-04-06 16:41:38 +0200

    Fixed model to run on tensorflow 1.7.0. However, no regularization.

--- tf_models/mnist.py
+++ tf_models/mnist.py
@@ -12,16 +12,20 @@ def create_model(input_tensor, mode, hyper_params):
 
         # First Conv Block
         conv1 = tf.layers.conv2d(inputs=x, filters=16, kernel_size=(3, 3), strides=(1, 1), name="conv1",
-                                 kernel_regularizer=tf.keras.regularizers.l2(l2_weight), activation=tf.nn.relu)
+                                 #kernel_regularizer=tf.keras.regularizers.l2(l2_weight),
+                                 activation=tf.nn.relu)
         conv2 = tf.layers.conv2d(inputs=conv1, filters=32, kernel_size=(3, 3), strides=(1, 1), name="conv2",
-                                 kernel_regularizer=tf.keras.regularizers.l2(l2_weight), activation=tf.nn.relu)
+                                 #kernel_regularizer=tf.keras.regularizers.l2(l2_weight),
+                                 activation=tf.nn.relu)
         pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=(2, 2), strides=(2, 2), name="pool2")
 
         # Second Conv Block
         conv3 = tf.layers.conv2d(inputs=pool2, filters=32, kernel_size=(3, 3), strides=(1, 1), name="conv3",
-                                 kernel_regularizer=tf.keras.regularizers.l2(l2_weight), activation=tf.nn.relu)
+                                 #kernel_regularizer=tf.keras.regularizers.l2(l2_weight),
+                                 activation=tf.nn.relu)
         conv4 = tf.layers.conv2d(inputs=conv3, filters=32, kernel_size=(3, 3), strides=(1, 1), name="conv4",
-                                 kernel_regularizer=tf.keras.regularizers.l2(l2_weight), activation=tf.nn.relu)
+                                 #kernel_regularizer=tf.keras.regularizers.l2(l2_weight),
+                                 activation=tf.nn.relu)
         pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=(2, 2), strides=(2, 2), name="pool4")
         if mode == tf.estimator.ModeKeys.TRAIN:
             pool4 = tf.layers.dropout(inputs=pool4, rate=hyper_params.arch.dropout_rate, name="drop4")
