04baf56cd281bdea01f1bb1ed5064fa4fd5cd004
Michael Fuerst <mail@michaelfuerst.de>
2018-04-27 14:44:16 +0200

    Added allow growth parameter.

--- starttf/utils/session_config.py
+++ starttf/utils/session_config.py
@@ -1,7 +1,9 @@
 import tensorflow as tf
 
+
 def get_default_config(gpu_memory_usage=0.75, allow_growth=False):
     config = tf.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = gpu_memory_usage
+    config.gpu_options.allow_growth = allow_growth
 
     return config
