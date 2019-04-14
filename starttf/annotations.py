4d646222d8ce5cf4bf8f3164996c376922b82a3d
Michael Fuerst <mail@michaelfuerst.de>
2019-04-14 13:10:22 +0200

    Fixed annotations to work correctly in classes.

--- starttf/annotations.py
+++ starttf/annotations.py
@@ -19,13 +19,25 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+from abc import ABC, abstractmethod
 
-class RunOnce(object):
+
+class ClassDecorator(ABC):
+    def __get__(self, obj, objtype):
+        """Support instance methods."""
+        import functools
+        return functools.partial(self.__call__, obj)
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        pass
+
+class RunOnce(ClassDecorator):
     def __init__(self, f):
         self.f = f
-        self.called = False
-    
+        self.called = {}
+
     def __call__(self, *args, **kwargs):
-        if not self.called:
-            self.called = True
+        if args[0] not in self.called:
+            self.called[args[0]] = True
             self.f(*args, **kwargs)
