d2f1227002462333d430e67a00d28b8412ce2bf5
Michael Fuerst <mail@michaelfuerst.de>
2019-05-20 23:49:18 +0200

    Moved sequences to starttf, since they are actually rather specific to tensorflow than general.

--- starttf/data/csv_sequence.py
+++ starttf/data/csv_sequence.py
@@ -20,6 +20,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from starttf.data.prepare import easy_tfrecord_prepare, create_input_fn
+import pandas as pd
+
 from starttf.data.simple_sequence import Sequence
-from starttf.data.csv_sequence import CSVSequence
+
+
+class CSVSequence(Sequence):
+    def __init__(self, hyperparams, phase, preprocess_fn=None, augmentation_fn=None):
+        super().__init__(hyperparams, phase, preprocess_fn, augmentation_fn)
+        file_name = hyperparams.problem.filename
+        feature_name_list = hyperparams.problem.feature_name_list
+        label_name_list = hyperparams.problem.label_name_list
+        df = pd.read_csv(file_name)
+
+        self.features = df.loc[:, feature_name_list].values
+        self.labels = df.loc[:, label_name_list].values
+
+    def num_samples(self):
+        return len(self.features)
+
+    def get_sample(self, idx):
+        return {"feature": self.features[idx]}, {"label": self.labels[idx]}
